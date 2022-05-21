264537b1da99bf48f3fc68a6f617abe62a034746
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-05-21 16:59:50 -0500

    Add loading indicator on Paper component

--- components/Paper.tsx
+++ components/Paper.tsx
@@ -1,16 +1,23 @@
+import { Loading, Row } from "@nextui-org/react";
 import { Quote } from "../pages/api/generate";
 import styles from "../styles/Paper.module.css";
 import CutQuote from "./CutQuote";
 
 type PaperProps = {
   quotes: Quote[];
+  loading: boolean;
 };
 
-const Paper: React.FC<PaperProps> = ({ quotes }) => {
+const Paper: React.FC<PaperProps> = ({ quotes, loading }) => {
   return (
     <div className={styles.notepad}>
       <div className={styles.top}></div>
       <div className={styles.paper}>
+        {loading && (
+          <Row justify="center">
+            <Loading size="xl" />
+          </Row>
+        )}
         {quotes.map((q, i) => {
           if (q.message) {
             return <CutQuote key={i} quote={q} />;
