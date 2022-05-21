dd154b5030d1784fc012f039f9cf61b33bdd5f73
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-05-21 14:18:21 -0500

    Add grid for textarea

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,30 @@
+import { Button, Grid, Spacer, Textarea, useInput } from "@nextui-org/react";
 import type { NextPage } from "next";
-import { useState } from "react";
-import styles from "../styles/Home.module.css";
 import { findQuotes } from "../utils/api";
 
 const Home: NextPage = () => {
-  const [input, setInput] = useState("");
+  const { value: message, bindings } = useInput("");
 
   const fetchResults = async () => {
-    const res = await findQuotes(input);
+    const res = await findQuotes(message);
     console.log(res);
   };
 
   return (
-    <div className={styles.container}>
-      <input onChange={(ev) => setInput(ev.target.value)} />
-      <button onClick={fetchResults}>Submit</button>
-    </div>
+    <Grid.Container gap={2} justify="center">
+      <Grid xs={12} sm={4} direction="column">
+        <Textarea
+          placeholder="Type your message here"
+          style={{ minHeight: "25vh" }}
+          {...bindings}
+        />
+        <Spacer y={0.5} />
+        <Button style={{ maxWidth: "5em" }} size="sm" onClick={fetchResults}>
+          Submit
+        </Button>
+      </Grid>
+      <Grid xs={12} sm={8}></Grid>
+    </Grid.Container>
   );
 };
 
