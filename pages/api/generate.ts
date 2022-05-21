db009c0afe8fc7313b9cbc37c3a02c4b5eec6f98
Connor Fogarty <connorflynnfogarty@gmail.com>
2022-05-21 13:41:05 -0500

    Optimize generation algorithm

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -6,48 +6,57 @@ type Quote = {
   message?: string;
 };
 
+/** maximum number of words searched for */
+const MAX_PHRASE_LENGTH = 3;
+
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Quote[]>
 ) {
   // fetch list of words from message
-  const message: string = req.body["message"];
+  const message: string = req.body.data["message"];
   const words = message
     .split(" ")
     .filter((str) => str.replace(/\s/g, "") !== "");
 
   const quotes: Quote[] = [];
 
-  // starting with the largest possible message (every word joined)
+  // starting with the largest possible message (max phrase length)
   // search for substrings of quotes with the message
-  let end = words.length;
-  while (end !== 0) {
-    for (let i = 0; i < end; i++) {
+  for (let i = 0; i < words.length; i++) {
+    for (
+      let end = Math.min(i + MAX_PHRASE_LENGTH, words.length);
+      end > i;
+      end--
+    ) {
       let mess = words.slice(i, end).join(" ");
-  
+
+      // remove capitalization, punctuation, and extra spaces
+      mess = mess.toLowerCase();
+      mess = mess.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
+      mess = mess.replace(/\s{2,}/g, " ");
+
       const fetchedQuotes = await supabase
         .from("quotes")
         .select("message")
         .like("message", `% ${mess} %`);
-  
+
       // quote exist with matching substring
       if (fetchedQuotes.data?.length) {
         const quote = fetchedQuotes.data[0];
-  
+
         quotes.push({
           message: quote,
           substring: mess,
         });
-  
-        end = i;
+
+        i = end - 1;
       }
       // reached end without finding matching substring
       else if (i === end - 1) {
         quotes.push({
           substring: words[i],
         });
-  
-        end = i;
       }
     }
   }
